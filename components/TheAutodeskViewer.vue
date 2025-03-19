0e878a173a0e822c975bdcc58d3eda9ea4813dba
AzureAD\EstherCidVara <ecid@hiberus.com>
2025-03-19 11:39:34 +0100

    add the BaseExtension and the lateral menu

--- components/TheAutodeskViewer.vue
+++ components/TheAutodeskViewer.vue
@@ -22,6 +22,7 @@ let globalViewer;
 onMounted(() => {
   let viewer = null;
   window.initAutodeskViewer = () => {
+    createBaseExtension();
     const options = {
       env: "AutodeskProduction",
       getAccessToken: async (onSuccess) => {
@@ -38,9 +39,11 @@ onMounted(() => {
     };
 
     Autodesk.Viewing.Initializer(options, () => {
+      loadBaseExtension();
       const viewerContainer = document.getElementById("viewer-container");
       viewer = new Autodesk.Viewing.GuiViewer3D(viewerContainer);
       viewer.start();
+      viewer.loadExtension("BaseExtension");
       globalViewer = viewer;
 
       // TODO - Añadir la urn de un modelo subido a US
@@ -51,6 +54,29 @@ onMounted(() => {
         onDocumentLoadFailure
       );
     });
+
+    const loadBaseExtension = () => {
+      class BaseExtension extends window.Autodesk.Viewing.Extension {
+        constructor(viewer, options) {
+          super(viewer, options);
+        }
+
+        load() {
+          console.log("BaseExtension loaded");
+          return true;
+        }
+
+        unload() {
+          console.log("BaseExtension unloaded");
+          return true;
+        }
+      }
+
+      window.Autodesk.Viewing.theExtensionManager.registerExtension(
+        "BaseExtension",
+        BaseExtension
+      );
+    };
 
     const onDocumentLoadSuccess = (doc) => {
       console.log(
@@ -73,8 +99,86 @@ onMounted(() => {
   };
 });
 
-</script>
+const createBaseExtension = () => {
+  class BaseExtension extends window.Autodesk.Viewing.Extension {
+    constructor(viewer, options) {
+      super(viewer, options);
+      this._onObjectTreeCreated = (ev) => this.onModelLoaded(ev.model);
+      this._onSelectionChanged = (ev) =>
+        this.onSelectionChanged(ev.model, ev.dbIdArray);
+      this._onIsolationChanged = (ev) =>
+        this.onIsolationChanged(ev.model, ev.nodeIdArray);
+    }
 
+    load() {
+      this.viewer.addEventListener(
+        Autodesk.Viewing.OBJECT_TREE_CREATED_EVENT,
+        this._onObjectTreeCreated
+      );
+      this.viewer.addEventListener(
+        Autodesk.Viewing.SELECTION_CHANGED_EVENT,
+        this._onSelectionChanged
+      );
+      this.viewer.addEventListener(
+        Autodesk.Viewing.ISOLATE_EVENT,
+        this._onIsolationChanged
+      );
+      console.log("BaseExtension loaded");
+      return true;
+    }
+
+    unload() {
+      this.viewer.removeEventListener(
+        Autodesk.Viewing.OBJECT_TREE_CREATED_EVENT,
+        this._onObjectTreeCreated
+      );
+      this.viewer.removeEventListener(
+        Autodesk.Viewing.SELECTION_CHANGED_EVENT,
+        this._onSelectionChanged
+      );
+      this.viewer.removeEventListener(
+        Autodesk.Viewing.ISOLATE_EVENT,
+        this._onIsolationChanged
+      );
+      console.log("BaseExtension unloaded");
+      return true;
+    }
+    async onModelLoaded(model) {
+      // this.overrideFirstPersonButtonAction()
+      // const tree = await this.getModelStructure(model);
+      // console.log("Model Object Tree: ", tree);
+      this.removeFirstPersonButton();
+      console.log("ESTE ES EL MODEL: ", model);
+    }
+
+    onSelectionChanged(model, dbids) {}
+
+    onIsolationChanged(model, dbids) {}
+
+    removeFirstPersonButton() {
+      this.viewer.toolbar
+        .getControl("navTools")
+        .removeControl("toolbar-bimWalkTool");
+    }
+  }
+
+  window.Autodesk.Viewing.theExtensionManager.registerExtension(
+    "BaseExtension",
+    BaseExtension
+  );
+};
+</script>
 <template>
-  <div id="viewer-container" />
+  <div id="viewer-container" class="viewer-container" />
 </template>
+
+<style scoped>
+.viewer-container {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 250px; /* Adjust this value based on your sidebar width */
+  right: 0;
+  overflow: hidden;
+}
+</style>
---
+++ components/TheLateralMenu.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <h1>Holitas de pollo</h1>
+    </div>
+</template>
+
+<style scoped>
+/* Agrega tus estilos aquí */
+</style>
